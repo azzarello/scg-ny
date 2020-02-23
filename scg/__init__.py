504d2e123bccc3bf39a1114ea7fed19dc8ea991f
Connor Azzarello <connor.azzarello@gmail.com>
2020-02-23 09:28:39 -0800

    Changed __init__.py
    Co-authored-by: Christian Yang <kychris1999@hotmail.com>
    Co-authored-by: Teddy Nguyen <tpnguyen1@scu.edu>
    Co-authored-by: Anthony Chen <anthonychen2089@gmail.com>

--- scg/__init__.py
+++ scg/__init__.py
@@ -1,13 +1,2 @@
-__all__ = ['dash', 'flask', 'dash-core-components', 'dash-html-components', 'pandas']
-
-import pandas as pd
-import flask
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
-from datetime import datetime
-from pprint import pprint
-
-from dash.dependencies import Input, Output, State
-
-from func import generate_crime_entry, generate_ny_counties, parse_charges, output_answers
+import scg.app
+import scg.func
