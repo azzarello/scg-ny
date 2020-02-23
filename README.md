919d298dc7d6bcf5a18b81ed1aa2af36271c9791
Connor Azzarello <connor.azzarello@gmail.com>
2020-02-22 22:21:04 -0800

    Approaching MVP, time to sleep tho
    Co-authored-by: Christian Yang <kychris1999@hotmail.com>
    Co-authored-by: Teddy Nguyen <tpnguyen1@scu.edu>
    Co-authored-by: Anthony Chen <anthonychen2089@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# scg-ny
+# SecondChance
+
+SecondChance allows users to easily determine whether a crime can be expunged.
+
+## How It Works
+
+Follow the following link to access the web application:
+
+Since expungement varies by state, SecondChance updates the form based on the selected state and county.
+
+
+
--- app.py
+++ app.py
@@ -4,12 +4,16 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from datetime import datetime
+from pprint import pprint
 
 from dash.dependencies import Input, Output, State
 
-from func import crime_entry_div, generate_crime_entry
+from func import generate_crime_entry, generate_ny_counties, parse_charges, output_answers
 
-external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
+external_stylesheets = [
+    "https://codepen.io/chriddyp/pen/bWLwgP.css"]
+
+# "https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
 
 flask_server = flask.Flask(__name__)
 app = dash.Dash(__name__, server=flask_server,
@@ -20,28 +24,97 @@ app = dash.Dash(__name__, server=flask_server,
 app.config['suppress_callback_exceptions'] = True
 app.scripts.config.serve_locally = True
 
+ny_counties = generate_ny_counties()
+
+
 app.layout = html.Div(children=[
     html.Div(html.H2(
-        children='The Second Chance Gap', className='nine columns')),
+        children='The Second Chance Gap'), style={"text-align": "center"}),
 
     html.Div(children=[
-        dcc.Dropdown(id='state-dropdown',
-                     options=[
-                         {'label': 'New York', 'value': 'NY'},
-                         {'label': 'Washington State',
-                          'value': 'WA'}
-                     ],
-                     value='NY',
-                     className='six columns'),
-        dcc.Input(
-            placeholder='Enter the number of crimes committed...',
-            type='number',
-            value='0',
-            id='crimes-committed-input'
-        )]),
 
-    html.Div(id='charges-div', children=generate_crime_entry(2))
-])
+        html.Div(children=[
+            html.Div(children=html.P(), className="three columns"),
+            html.Div(children=[
+                dcc.Dropdown(id='state-dropdown',
+                             options=[
+                                {'label': 'New York', 'value': 'NY'},
+                                {'label': 'Washington State',
+                                 'value': 'WA'}
+                             ]),
+                dcc.Dropdown(id='ny-county-dropdown',
+                             options=ny_counties,
+                             style={"display": "none"}
+                             ),
+            ], className="six columns"),
+            html.Div(children=html.P(), className="three columns"), ],
+            className="row"
+        ),
+
+
+        html.Br(),
+
+        html.Div(children=[
+            html.Div(children=html.P(), className="three columns"),
+            html.Div(children=[
+                html.P(children='How many crimes did you commit?  ',
+                       style={"display": "inline"}),
+                dcc.Input(
+                    type='number',
+                    value=1,
+                    id='crimes-committed-input',
+                    min=1
+                )
+            ], className="six columns"),
+            html.Div(children=html.P(), className="three columns"), ],
+            className="row"
+        ),
+
+
+    ]),
+
+
+    html.Div(id='charges-div', children=generate_crime_entry(1),
+             className="row", style={"text-align": "center"}),
+
+
+
+
+    html.Br(),
+    html.Div(children=html.Button('Submit', id='submit-crimes-button',
+                                  className="button-primary"), style={"text-align": "center"}),
+
+
+    html.Hr(),
+    html.Div(children=[
+        html.H1(children="CONGRATS! You are free now",
+                style={"text-align": "center"}, id="result"),
+        html.Div(children=[
+            html.Div(children=[
+                html.P(
+                    children="DO YOU HAVE MORE THAN TWO (2) CRIMINAL CONVICTIONS (MISDEMEANOR OR FELONY)?"),
+                html.P(children="DO YOU HAVE MORE THAN ONE FELONY CONVICTION?"),
+                html.P(
+                    children="HAVE LESS THAN TEN YEARS PASSED SINCE YOUR LAST CRIMINAL CONVICTION?"),
+                html.P(children="ARE YOU REQUIRED TO REGISTER AS A SEX OFFENDER?"),
+                html.P(children="ARE YOU APPLYING TO SEAL AN INELIGIBLE OFFENSE?"),
+                html.P(children="DO YOU CURRENTLY HAVE AN OPEN CRIMINAL CASE?"),
+            ], className="ten columns"),
+            html.Div(children=[
+                html.P(
+                    children="√", id="criminal_convictions"),
+                html.P(children="√", id="felony_conviction"),
+                html.P(
+                    children="X", id="ten_years_period"),
+                html.P(children="√", id="sex_offender"),
+                html.P(children="√", id="ineligible_offense"),
+                html.P(children="√", id="open_criminal_case"),
+            ], className="two columns", style={"text-align": "right"}),
+        ], className="row"),
+    ], id="eligibility_info", style={'display': 'none'}),
+
+
+], className="container")
 
 
 @app.callback(Output('charges-div', 'children'),
@@ -50,10 +123,58 @@ def generate_case_entry_div(value):
     return generate_crime_entry(value)
 
 
-@app.callback(Output('output', 'children'),
+@app.callback(Output('ny-county-dropdown', 'style'),
               [Input('state-dropdown', 'value')])
 def update_output_component(dropdown_value):
-    return dropdown_value
+    if dropdown_value == 'NY':
+        return {}
+    else:
+        return {'display': 'none'}
+
+
+"""
+@app.callback([Output('eligibility_info', 'children'),               
+               ],
+              [Input('submit-crimes-button', 'n_clicks')])
+def showResult(n):
+    if (n > 0):
+        return {}
+    else:
+        return {"display" : "none"}
+
+"""
+
+
+@app.callback([Output('criminal_convictions', 'children'),
+               Output('felony_conviction', 'children'),
+               Output('ten_years_period', 'children'),
+               Output('sex_offender', 'children'),
+               Output('ineligible_offense', 'children'),
+               Output('open_criminal_case', 'children'),
+               Output('result', 'children'),
+               Output('eligibility_info', 'style')],
+              [Input('submit-crimes-button', 'n_clicks')],
+              [State('charges-div', 'children')])
+def update_crimes_store(n, charges):
+    """
+    if (n > 1):
+        result = parse_charges(charges)
+        eligibility, result_list = expunge_eligibility(_____result______)
+        return_message=""
+        if (eligibility):
+            return_message="Congrats!"
+        else:
+            return_message="Sorry!"
+        return result_list[0], result_list[1], result_list[2], result_list[3], result_list[4], result_list[5], return_message
+    """
+    if (n >= 1):
+        eligible_list, eligible = parse_charges(charges)
+        ret = [0, 0, 0, 0, 0, 0]
+        return_message = ''
+        ret[0], ret[1], ret[2], ret[3], ret[4], ret[5], return_message = output_answers(
+            eligible_list, eligible)
+        print(ret)
+        return ret[0], ret[1], ret[2], ret[3], ret[4], ret[5], return_message, {}
 
 
 if __name__ == '__main__':
