df6732f9dca785fc39f55e2c8ecccd437b7f7939
Connor Azzarello <connor.azzarello@gmail.com>
2020-02-23 14:01:46 -0800

    Wrong attribute fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:app --log-file "log.txt"
+web: gunicorn app:flask_server --log-file "log.txt"
--- scg/__init__.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-import flask
-import dash
-import dash_core_components as dcc
-import dash_html_components as html
